2231dc0b6c94c3f2ac2392a91889a64244b27a90
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-03-18 03:55:08 -0700

    TransactionAccount Subclass completed

--- src/TransactionAccount.java
+++ src/TransactionAccount.java
@@ -1,2 +1,31 @@
-public class TransactionAccount {
+public class TransactionAccount extends BankAccount{
+    //حداکثر مقدار قایل برداشت بیشتر از موجودی کاربر
+    private final double overDraftLimit = 500;
+
+    public AccountTransaction (String accountHolderName, String accountNumber, double initialBalance){
+        super(accountHolderName, accountNumber, initialBalance);
+    }
+
+    @Override
+    public void calculateInterest(){
+        System.out.println("There is no interest for this account.");
+    }
+
+    @Override
+    public void withdraw (double amount){
+        if (amount > 0) {
+            if(getBalance() + overDraftLimit >= amount){
+            double newBalance =getBalance() - amount;
+            setBalance(newBalance);
+            System.out.println(amount + "were withdrawal from yout account");
+        }
+        else
+            System.out.println("Your account balance is not sufficient");
+    }
+        else
+            System.out.println("Wrong amount!");
+
+        private void setBalance(double balance) {
+            this.balance = balance;
+        }
 }

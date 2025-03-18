cdd2de8829c7874bf9293c27f4401c7f73412597
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-03-18 02:51:36 -0700

    SavingAccount Subclass completed

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -1,2 +1,15 @@
-public class SavingAccount {
+public class SavingAccount extends BankAccount{
+
+    private final double interestRate = 0.03;
+
+    public SavingsAccount(String accountNumber, String accountHolderName, double initialBalance){
+        super(accountNumber, accountHolderName, initialBalance);
+    }
+
+    @Override
+    public void calculateInterest() {
+        double profit = interestRate * getBalance();
+        deposit(profit);
+    }
+
 }
